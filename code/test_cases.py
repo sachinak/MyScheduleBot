6fc3a6a7ba52e7a92adf6f6cd33e082569301baf
Connor Smith <connor.s1110@gmail.com>
2021-08-31 15:25:11 -0400

    Updating test cases

--- code/test_cases.py
+++ code/test_cases.py
@@ -2,20 +2,24 @@ from increment import *
 
 
 def test_answer1():
-    assert inc(3) == 5
+    assert inc(3) != 5
 
 
 def test_answer2():
-    assert inc(3) == 2
+    assert inc(3) != 2
 
 
 def test_answer3():
-    assert inc(3) == 3.1
+    assert inc(3) != 3.1
 
 
 def test_answer4():
-    assert inc(3) == 2.9
+    assert inc(3) != 2.9
 
 
 def test_answer5():
     assert inc(3) == 4
+
+
+def test_answer6():
+    assert inc(2.9) == 3.9
