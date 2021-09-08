67d3284cf2cc8d00a8e92d4840b5afc38a602dff
Connor Smith <83387006+ConnorS1110@users.noreply.github.com>
2021-09-08 01:05:42 +0000

    Implement DM bot feature

--- src/schedulebot.py
+++ src/schedulebot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 
 client = discord.Client()
@@ -8,8 +9,13 @@ client = discord.Client()
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
   channel = client.get_channel(884864860859531347) # Gets the channel ID of the "schedule-manager channel"
-  await channel.send("Hello! My name is Schedule Bot and I am here to help you plan your schedule!\n\n" + 
-  "Here are a list of commands you can use:\n" +
+  await channel.send("Hello! My name is Schedule Bot and I am here to help you plan your schedule!\n\n" +
+  "React to this message with a '⏰' (\:alarm_clock\:) reaction so I can direct message you!")
+
+@client.event
+async def on_reaction_add(reaction, user):
+  if reaction.emoji == '⏰':
+    await user.send("Here are a list of commands you can use:\n" +
   "add: Adds a single event\n" + "batch: Adds multiple events at once\n" + "delete: Deletes an event\n")
 
 @client.event
@@ -18,6 +24,6 @@ async def on_message(message):
     return
 
   if message.content.startswith("$hello"):
-    await message.channel.send("Hello")
+    await message.author.send("Hello")
 
 client.run(os.environ['TOKEN'])
