358b857fedee5da9ec3570a186391c7e16d99129
Connor Smith <connor.s1110@gmail.com>
2021-09-20 22:24:40 -0400

    Restructured Event objects to allow for end dates to events and updated implementation in AddEvent.py

--- src/Event.py
+++ src/Event.py
@@ -1,29 +1,100 @@
 class Event:
-    def __init__(self, name, hour, minutes, half, day_of_week, month, date_day, year, event_type, description):
+    def __init__(self, name, start_date, end_date, event_type, description):
         self.name = name
-        self.hour = hour
-        self.minutes = minutes
-        if half == None:
-            if hour >= 12:
-                print("there")
-                self.half = "pm"
-            else:
-                print("over there")
-                self.half = "am"
-        else:
-            self.half = half
-        self.day_of_week = day_of_week
-        print(self.day_of_week)
-        self.month = month
-        print(self.month)
-        self.date_day = date_day
-        print(self.date_day)
-        self.year = year
-        print(self.year)
+        self.start_date = start_date
+        self.end_date = end_date
         self.event_type = event_type
-        print(self.event_type)
-        if description != None:
-            self.description = description
-        else:
-            self.description = ""
-        print(self.description)
+        self.description = description
+
+    # Converts event object to a string
+    def __str__(self):
+        output = self.name + " " + str(self.start_date) + " " + \
+            str(self.end_date) + " " + self.event_type + " " + self.description
+        return output
+
+    # # Compares two events to see which one is further away (less than)
+    # def __lt__(self, other):
+    #     if self.year != other.year:
+    #         return self.year < other.year
+    #     else:
+    #         if self.month != other.month:
+    #             return self.month < other.month
+    #         else:
+    #             if self.date_day != other.date_day:
+    #                 return self.date_day < other.date_day
+    #             else:
+    #                 if self.hour != other.hour:
+    #                     return self.hour < other.hour
+    #                 else:
+    #                     return self.minutes < other.minutes
+
+    # # Compares two events to see which one is further away (less than or equal to)
+    # def __le__(self, other):
+    #     if self.year != other.year:
+    #         return self.year <= other.year
+    #     else:
+    #         if self.month != other.month:
+    #             return self.month <= other.month
+    #         else:
+    #             if self.date_day != other.date_day:
+    #                 return self.date_day <= other.date_day
+    #             else:
+    #                 if self.hour != other.hour:
+    #                     return self.hour <= other.hour
+    #                 else:
+    #                     return self.minutes <= other.minutes
+
+    # # Compares two events to see which one is closer (greater than)
+    # def __gt__(self, other):
+    #     if self.year != other.year:
+    #         return self.year > other.year
+    #     else:
+    #         if self.month != other.month:
+    #             return self.month > other.month
+    #         else:
+    #             if self.date_day != other.date_day:
+    #                 return self.date_day > other.date_day
+    #             else:
+    #                 if self.hour != other.hour:
+    #                     return self.hour > other.hour
+    #                 else:
+    #                     return self.minutes > other.minutes
+
+    # # Compares two events to see which one is closer (greater than or equal to)
+    # def __ge__(self, other):
+    #     if self.year != other.year:
+    #         return self.year >= other.year
+    #     else:
+    #         if self.month != other.month:
+    #             return self.month >= other.month
+    #         else:
+    #             if self.date_day != other.date_day:
+    #                 return self.date_day >= other.date_day
+    #             else:
+    #                 if self.hour != other.hour:
+    #                     return self.hour >= other.hour
+    #                 else:
+    #                     return self.minutes >= other.minutes
+
+    # # Compares two events to see if they are at the same time (equal to)
+    # def __eq__(self, other):
+    #     if self.year == other.year:
+    #         if self.month == other.month:
+    #             if self.date_day == other.date_day:
+    #                 if self.hour == other.hour:
+    #                     return self.minutes == other.minutes
+    #                 else:
+    #                     return False
+    #             else:
+    #                 return False
+    #         else:
+    #             return False
+    #     else:
+    #         return False
+
+    # # Compares two events to see if they are not at the same time
+    # def __ne__(self, other):
+    #     return not(self == other)
+
+    # def intersect(self, other):
+    #     return
--- src/functionality/AddEvent.py
+++ src/functionality/AddEvent.py
@@ -1,28 +1,127 @@
 import re
+from datetime import datetime
 from Event import Event
 
 
 async def add_event(client, message):
-    await message.author.send("Lets add an event!\n" + "Follow the format:\n\n" + "name hh:mm am/pm(if not using military time) day(optional) mm/dd/yyyy type description(optional)\n\n" +
-                              "Example: \"Job Interview 12:30 pm Thursday 09/09/2021 meeting\" adds an event called 'Job Interview' at 12:30 pm on Thursday September 9th, 2021 with type 'meeting' with no description")
+    event_array = []
+    await message.author.send("Lets add an event!\n" + "First give me the name of your event:")
     event_msg = await client.wait_for('message')  # Waits for user input
     event_msg = event_msg.content   # Strips message to just the text the user entered
-    # splits the users response into an array to be converted into an "Event" object
-    event_array = re.split('\s|:|/', event_msg)
-    # If a user did not enter "am" or "pm"
-    if event_array[3].lower() != "am" and event_array[3].lower() != "pm":
-        # a "None" element is inserted in its place
-        event_array.insert(3, None)
-    if 10 > len(event_array):       # If a description was not included
-        # a "None" element is added to its place at the end of the array
-        event_array.append(None)
+    event_array.append(event_msg)
+    await message.author.send("Now give me the start & end dates for you event.\n" +
+                              "If you don't use military time, make sure you include 'am' or 'pm' so I know when to schedule your event, otherwise I will assume am\n\n" +
+                              "Here is the format you should follow (Start is first, end is second):\n" + "mm/dd/yy hh:mm am/pm mm/dd/yy hh:mm am/pm")
+
+    event_dates = False
+    # A loop that keeps running until a user enters correct start and end dates for their event following the required format
+    # Adds start and end dates to the array if both are valid
+    while(not event_dates):
+        date_array = []
+        msg_content = ""
+        start_complete = False
+        end_complete = True
+        if message.author != client.user:
+            # Waits for user input
+            event_msg = await client.wait_for('message')
+            # Strips message to just the text the user entered
+            msg_content = event_msg.content
+            # Splits response to prepare data to be appended to event_array
+            date_array = re.split('\s', msg_content)
+
+        # Adds a leading 0 if the user forgets to add one for single digit hour times for start date
+        if ':' in date_array[1][0:2]:
+            date_array[1] = '0' + date_array[1]
+
+        # Checks to see if user entered am/pm for start/end dates and assumes am if nothing was entered and user isn't using military time
+        # If military time was used, it adds an empty string into the array in the place of an am/pm
+        if (date_array[2].lower() != "am" and date_array[2].lower() != "pm"):
+            if int(date_array[1][0:2]) < 12:
+                date_array.insert(2, "AM")
+            else:
+                date_array.insert(2, "")
+
+        # Adds a leading 0 if the user forgets to add one for single digit hour times for end date
+        if ':' in date_array[4][0:2]:
+            date_array[4] = '0' + date_array[4]
+
+        if (date_array[5].lower() != "am" and date_array[5].lower() != "pm"):
+            if int(date_array[4][0:2]) < 12:
+                date_array.insert(5, "AM")
+            else:
+                date_array.insert(5, "")
+
+        # Tries to create the state_date datetime object
+        try:
+            if int(date_array[1][0:2]) >= 12 and date_array[2] == "":
+                start_date = datetime.strptime(
+                    date_array[0] + " " + date_array[1] + " " + date_array[2], "%m/%d/%y %H:%M")
+                start_complete = True
+                print("Created start_date object: " + str(start_date))
+            else:
+                start_date = datetime.strptime(
+                    date_array[0] + " " + date_array[1] + " " + date_array[2], "%m/%d/%y %I:%M %p")
+                start_complete = True
+                print("Created start_date object: " + str(start_date))
+        except Exception as e:
+            print(e)
+            await message.author.send("Looks like you didn't enter your start dates correctly.")
+            start_complete = False
+            date_array = []
+            event_msg = ""
+
+        # Tries to create the end_date datetime object
+        try:
+            if int(date_array[4][0:2]) >= 12 and date_array[5] == "":
+                end_date = datetime.strptime(
+                    date_array[3] + " " + date_array[4] + " " + date_array[5], "%m/%d/%y %H:%M")
+                end_complete = True
+                print("Created end_date object: " + str(end_date))
+            else:
+                end_date = datetime.strptime(
+                    date_array[3] + " " + date_array[4] + " " + date_array[5], "%m/%d/%y %I:%M %p")
+                end_complete = True
+                print("Created end_date object: " + str(end_date))
+        except Exception as e:
+            print(e)
+            await message.author.send("Looks like you didn't enter your end dates correctly.")
+            end_complete = False
+            date_array = []
+            event_msg = ""
+
+        # If both datetime objects were successfully created, they get appended to the list and exits the while loop
+        if start_complete and end_complete:
+            print("Both date objects created")
+            event_dates = True
+            event_array.append(start_date)
+            event_array.append(end_date)
+
+        # If both objects were unsuccessfully created, the bot notifies the user and the loop starts again
+        else:
+            await message.author.send("Make sure you follow this format(Start is first, end is second): mm/dd/yy hh:mm am/pm mm/dd/yy hh:mm am/pm")
+            date_array = []
+            msg_content = ""
+
+    await message.author.send("Tell me what type of event this is. Here are a list of event types I currently know:")
+    event_msg = await client.wait_for('message')  # Waits for user input
+    event_msg = event_msg.content   # Strips message to just the text the user entered
+    event_array.append(event_msg)
+    await message.author.send("Any additional description you want me to add about the event? If not, enter 'done'")
+    event_msg = await client.wait_for('message')  # Waits for user input
+    event_msg = event_msg.content   # Strips message to just the text the user entered
+    if event_msg.lower() == "done":
+        event_array.append("")
+    else:
+        event_array.append(event_msg)
 
     # Tries to create an Event object from the user input
     try:
-        current = Event(event_array[0], int(event_array[1]), int(event_array[2]), event_array[3], event_array[4], int(
-            event_array[5]), int(event_array[6]), int(event_array[7]), event_array[8], event_array[9])
+        current = Event(event_array[0], event_array[1],
+                        event_array[2], event_array[3], event_array[4])
         await message.author.send("Your event was successfully created!")
+        print(current)
         return current
-    except:
+    except Exception as e:
         # Outputs an error message if the event could not be created
-        await message.author.send("There was an error creating your event. Make sure your formatting is correct.")
+        print(e)
+        await message.author.send("There was an error creating your event. Make sure your formatting is correct and try creating the event again.")
