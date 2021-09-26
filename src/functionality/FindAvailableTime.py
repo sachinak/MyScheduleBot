8d40f35efe0e65e850df44591e687d376285d416
vasuagrawal19 <vasuagwl@gmail.com>
2021-09-25 23:57:51 -0400

    Updated the AvailableTime function for discord.ext

--- src/functionality/FindAvailableTime.py
+++ src/functionality/FindAvailableTime.py
@@ -1,31 +1,24 @@
-import re
-from config import EventTypes
-from config import SetTime
+import os
 
-async def find_availableTime(client, message):
+async def find_avaialbleTime(ctx, client):
 
-    st = "Let's find time for your event. Enter the event type from the following :-\n"
-    st1 = ' '
-    for val in EventTypes:
-        st1+=val+'\n'
+    channel = await ctx.author.create_dm()
+    # print(ctx.author.id)
+    def check(m):
+        return m.content is not None and m.channel == channel and m.author == ctx.author
 
-    await message.author.send(st+st1)
-    event_msg = await client.wait_for('message')  # Waits for user input
-    event_msg = event_msg.content   # Strips message to just the text the user entered
+    await channel.send("Let's find time for your event. Enter the Event Type:-")
+    event_msg = await client.wait_for("message", check=check)  # Waits for user input
+    event_msg = event_msg.content  # Strips message to just the text the user entered
     print(event_msg)
 
-    get_val = SetTime[event_msg]
-    startTime = get_val[0] if get_val[0]<12 else get_val[0]-12
-    range = 'am' if get_val[0]<12 else 'pm'
-    timeAvailable = 'You have '+event_msg+' time available between '+str(startTime)+' '+range
-    endTime = get_val[1] if get_val[1]<12 else get_val[1]-12
-    range = 'am' if get_val[1]<12 else 'pm'
-    timeMessage = timeAvailable+' to '+str(endTime)+' '+range
+#    get_val = SetTime[event_msg]    
+#    startTime = get_val[0] if get_val[0]<12 else get_val[0]-12
+#    range = 'am' if get_val[0]<12 else 'pm'
+#    timeAvailable = 'You have '+event_msg+' time available between '+str(startTime)+' '+range
+#    endTime = get_val[1] if get_val[1]<12 else get_val[1]-12
+#    range = 'am' if get_val[1]<12 else 'pm'
+#    timeMessage = timeAvailable+' to '+str(endTime)+' '+range
 
-    await message.author.send(timeMessage)
+#    await message.author.send(timeMessage)
 
-    try:
-        await message.author.send("Your event was successfully created!")
-    except:
-        # Outputs an error message if the event could not be created
-        await message.author.send("There was an error creating your event. Make sure your formatting is correct.")
