301ec96f586ff1a8f53244e7f6013573f739aba0
Rahul <rahulramakris@gmail.com>
2021-09-29 21:23:35 -0400

    add date checking test cases

--- test/test_highlight.py
+++ test/test_highlight.py
@@ -7,14 +7,14 @@ import pytest
 import datetime
 
 from random import randint
-from functionality.highlights import check_start_or_end
+from functionality.highlights import check_start_or_end, convert_to_12
 
 
 NUM_ITER = 1000
 
 
 # Generate one random datetime object between an uniform range
-def random_datetime(start = 2020, end = 2025):
+def random_date(start = 2020, end = 2025):
     # Determine all posible days between the start and end
     start = datetime.date(start, 1, 1)
     end = datetime.date(end, 12, 31)
@@ -23,16 +23,43 @@ def random_datetime(start = 2020, end = 2025):
     choices = end - start
     chosen_day = randint( 0, choices.days )
 
-    # Randomly pick a time
-    h = randint(0, 23)
-    m = randint(0, 59)
-
     # Get the random date
     date = start + datetime.timedelta(days = chosen_day)
 
     # add time
-    return datetime.datetime(date.year, date.month, date.day, h, m)
+    return str(datetime.datetime(date.year, date.month, date.day)).split()[0]
 
 
+def test_start_and_end():
+    day1 = random_date()
+    day2 = day1
 
-print(random_datetime())
+    today = day1
+    assert check_start_or_end([day1, day2], today) == 1
+
+def test_ends_later():
+    day1 = random_date()
+    day2 = random_date()
+
+    day1, day2 = min(day1, day2), max(day1, day2)
+    today = day1
+
+    assert check_start_or_end([day1, day2], today) == 2
+
+def test_started_earlier():
+    day1 = random_date()
+    day2 = random_date()
+
+    day1, day2 = min(day1, day2), max(day1, day2)
+    today = day2
+
+    assert check_start_or_end([day1, day2], today) == 3
+
+def test_no_event():
+    day1 = random_date()
+    day2 = random_date()
+
+    day1, day2 = min(day1, day2), max(day1, day2)
+    today = random_date(2018, 2019)
+
+    assert check_start_or_end([day1, day2], today) == 0
