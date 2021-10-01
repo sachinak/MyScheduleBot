3f1c7a69bbef550b1af94fee643ea4020ddcefd5
ashwith161 <89534400+ashwith161@users.noreply.github.com>
2021-09-30 21:37:05 -0400

    created functions delete_type and print_type

--- src/functionality/delete_event_type.py
+++ src/functionality/delete_event_type.py
@@ -4,6 +4,53 @@ import csv
 from pathlib import Path
 from types import TracebackType
 
+def delete_type(rows, msg_content):
+    """
+    Function:
+        delete_type
+    Description:
+        A existing event type is deleted from the user's calendar file   
+    Input:
+        rows: lsit of lines in calendar
+        msg_content: event type to be deleted
+    Output:
+        - A existing event type is deleted from the user's calendar file 
+    """
+    flag=0
+    line_number = 0
+    new_row=[]
+    for line in rows:
+        if str(line[0]) == msg_content :
+            flag=1
+        else:
+            new_row.append(line)
+            line_number+=1
+    temp=[new_row, flag, line_number]
+    return temp
+
+def print_type(calendar_lines):
+    """
+    Function:
+        print_type
+    Description:
+        Sends the lsit of all event type in csv file
+    Input:
+        calendar_lines - object for csv.reader for user's calendar file
+    Output:
+        -  Sends the lsit of all event types as list and string
+    """
+    list_types=''
+    rows = []
+    #print (calendar_lines)
+    for line in calendar_lines:
+
+        if len(line) > 0:
+            rows.append(line)
+            list_types= list_types + "\nEvent Type: " + str(line[0]) + " prefered time range from " + str(line[1]) +" to " +str(line[2])
+    temp1= [rows, str(list_types)]
+    return temp1
+
+
 async def delete_event_type(ctx, client):
     """
     Function:
@@ -37,17 +84,13 @@ async def delete_event_type(ctx, client):
             ) as calendar_lines:
                 calendar_lines = csv.reader(calendar_lines, delimiter=",")
                 fields = next(calendar_lines)  # The column headers will always be the first line of the csv file
-                rows = []
-                new_row=[]
-                line_number = 0
-                flag=0
-                list_types=''
-                #printing the list of event type
-                for line in calendar_lines:
-                    if len(line) > 0:
-                        rows.append(line)
-                        list_types= list_types + "\nEvent Type: " + str(line[0]) + " prefered time range from " + str(line[1]) +" to " +str(line[2])
                 
+                new_row=[]
+                #printing the list of event type
+                temp1=print_type(calendar_lines)
+                rows = temp1[0]
+                list_types=temp1[1]
+
                 if list_types=='':
                     await channel.send("You have not created any events type yet!!")
                 else:
@@ -60,15 +103,16 @@ async def delete_event_type(ctx, client):
                     msg_content = str(event_msg.content)
 
                     # Searching and deleting the event type
-                    for line in rows:
-                        if str(line[0]) == msg_content :
-                            await channel.send("Event type " + msg_content +" has been deleted.")
-                            flag=1
-                        else:
-                            new_row.append(line)
-                            line_number+=1
+                    temp= delete_type(rows, msg_content)
+                    new_row=temp[0]
+                    flag= temp[1]
+                    line_number = temp[2]
+                
                     if flag==0:
                         await channel.send("Event type does not exist")
+                    if flag==1:
+                        await channel.send("Event type " + msg_content +" has been deleted.")
+
                  # Open current user's calendar file for writing
                 with open(
                     os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + str(filename) + ".csv", "w", newline=""
@@ -90,4 +134,3 @@ async def delete_event_type(ctx, client):
         await channel.send(
             "There was an error deleting your event."
         )
-
