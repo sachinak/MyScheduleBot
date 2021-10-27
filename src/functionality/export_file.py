cbdbdff428d7b2e38b5cd741f6068175111b2baf
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-10-27 14:35:37 -0400

    Create export_file.py

---
+++ src/functionality/export_file.py
@@ -0,0 +1,19 @@
+import os
+import csv
+from shared_functions import create_event_file
+
+
+async def export_file(ctx, client):
+    channel = await ctx.author.create_dm()
+    print(ctx.author.id)
+
+    def check(m):
+        return m.content is not None and m.channel == channel and m.author == ctx.author
+
+    user = str(ctx.author.id)
+
+    # Checks if the calendar csv file exists, and creates it if it does not
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + str(user) + ".csv"):
+        create_event_file(user)
+
+    channel.send("test")
