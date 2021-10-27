dfe29f9c01bc22fa3995aecc2e4981b0e8d2d453
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-10-27 15:17:21 -0400

    Added exportfile functionality

--- src/functionality/export_file.py
+++ src/functionality/export_file.py
@@ -1,19 +1,20 @@
 import os
 import csv
+import discord
 from functionality.shared_functions import create_event_file
 
 
-async def export_file(ctx, client):
+async def export_file(ctx):
     channel = await ctx.author.create_dm()
     print(ctx.author.id)
 
     def check(m):
         return m.content is not None and m.channel == channel and m.author == ctx.author
 
-    user = str(ctx.author.id)
+    user_id = str(ctx.author.id)
 
     # Checks if the calendar csv file exists, and creates it if it does not
-    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + str(user) + ".csv"):
-        create_event_file(user)
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv"):
+        create_event_file(user_id)
 
-    channel.send("test")
+    await channel.send(file=discord.File(os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv"))
