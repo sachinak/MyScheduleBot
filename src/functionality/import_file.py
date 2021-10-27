d4d2dd4d410224f969527b3aea38075d72a20aca
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-10-27 18:49:03 -0400

    Added verify_csv function

--- src/functionality/import_file.py
+++ src/functionality/import_file.py
@@ -5,6 +5,32 @@ import pandas as pd
 from functionality.shared_functions import create_event_tree, create_type_tree, add_event_to_file, turn_types_to_string
 from Event import Event
 
+def verify_csv(data):
+    """
+    Function:
+        verify_csv
+    Description:
+        Verifies that CSV data retrieved through pandas matches the expected format
+    Input:
+        data - A Pandas Dataframe of data pulled from a CSV
+    Output:
+        - True if the data matches the expectation, false otherwise
+    """
+
+    if data.columns[0] != "ID":
+        return False
+    if data.columns[1] != "Name":
+        return False
+    if data.columns[2] != "Start Date":
+        return False
+    if data.columns[3] != "End Date":
+        return False
+    if data.columns[4] != "Type":
+        return False
+    if data.columns[5] != "Notes":
+        return False
+
+    return True
 
 async def import_file(ctx, client):
     channel = await ctx.author.create_dm()
