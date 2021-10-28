0b211235e22152c9d2f6b4c7ff09085f20820290
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-10-28 15:31:47 -0400

    Fixed test (for real this time)

--- test/test_import_file.py
+++ test/test_import_file.py
@@ -11,14 +11,14 @@ def test_time():
     old_time = "1998-05-08 10:30:00"
     new_time = convert_time(old_time)
 
-    assert "05/08/98 10:30 am" == new_time
+    assert "05/08/98 10:30am" == new_time
 
 
 def test_time_pm():
     old_time = "1998-05-08 18:30:00"
     new_time = convert_time(old_time)
 
-    assert "05/08/98 6:30 pm" == new_time
+    assert "05/08/98 6:30pm" == new_time
 
 
 def test_working_csv():
@@ -29,7 +29,7 @@ def test_working_csv():
             'Type': '',
             'Notes': ''}
 
-    table = pd.DataFrame(data=d)
+    table = pd.DataFrame(data=data)
 
     assert verify_csv(table)
 
@@ -42,6 +42,6 @@ def test_typo_csv():
             'Type': '',
             'Notes': ''}
 
-    table = pd.DataFrame(data=d)
+    table = pd.DataFrame(data=data)
 
     assert not verify_csv(table)
