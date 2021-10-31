ca5e15ccab961f6d9592c1ec88374349c1494299
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-10-31 18:26:24 -0400

    Added an export file test

---
+++ test/test_export_file.py
@@ -0,0 +1,55 @@
+import sys, os
+import asyncio
+import discord
+import discord.ext.commands as commands
+import discord.ext.test as test
+import threading
+import time
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/../src"))
+
+import pytest
+from datetime import datetime
+
+from functionality.export_file import export_file  # type: ignore
+
+
+@pytest.fixture
+def client(event_loop):
+    c = discord.Client(loop=event_loop)
+    test.configure(c)
+    return c
+
+
+@pytest.fixture
+def bot(request, event_loop):
+    intents = discord.Intents.default()
+    intents.members = True
+    b = commands.Bot("!", loop=event_loop, intents=intents)
+
+    marks = request.function.pytestmark
+    mark = None
+    for mark in marks:
+        if mark.name == "cogs":
+            break
+
+    if mark is not None:
+        for extension in mark.args:
+            b.load_extension("tests.internal." + extension)
+
+    test.configure(b)
+    return b
+
+
+@pytest.mark.asyncio
+async def test_export_file(bot, client):
+    guild = bot.guilds[0]
+    channel = guild.text_channels[0]
+    message = await channel.send("!exportfile")
+
+    assert len(bot.private_channels) == 0
+
+    thread = threading.Thread(target=export_file, args=(message, bot), daemon=True)
+    thread.start()
+
+    time.sleep(.5)
