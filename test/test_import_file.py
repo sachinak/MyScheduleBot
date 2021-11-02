54162a487ce4e48dbda847a28e0100e707d91657
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-11-02 17:37:15 -0400

    Added ics file as a string for testing

--- test/test_import_file.py
+++ test/test_import_file.py
@@ -14,13 +14,66 @@ import time
 from schedulebot import importfile
 from functionality.import_file import verify_csv, convert_time, import_file, get_ics_data
 
-'''
-bot_test = commands.Bot(command_prefix="!")
-
-@bot_test.command()
-def unit_import(ctx):
-    importfile(ctx)
-'''
+ICS_STRING = "BEGIN:VCALENDAR\n" \
+             "PRODID:-//Google Inc//Google Calendar 70.9054//EN\n" \
+             "VERSION:2.0\n" \
+             "CALSCALE:GREGORIAN\n" \
+             "METHOD:PUBLISH\n" \
+             "X-WR-CALNAME:Birthdays\n" \
+             "X-WR-TIMEZONE:UTC\n" \
+             "X-WR-CALDESC:Displays birthdays\, anniversaries\, and other event dates of people in Google Contacts.\n" \
+             "BEGIN:VEVENT\n" \
+             "DTSTART;VALUE=DATE:20200508\n" \
+             "DTEND;VALUE=DATE:20200509\n" \
+             "DTSTAMP:20211102T171105Z\n" \
+             "UID:2020_BIRTHDAY_self@google.com\n" \
+             "X-GOOGLE-CALENDAR-CONTENT-DISPLAY:chip\n" \
+             "X-GOOGLE-CALENDAR-CONTENT-ICON:https://calendar.google.com/googlecalendar/i\n" \
+             " mages/cake.gif\n" \
+             "CLASS:PUBLIC\n" \
+             "CREATED:20211029T145140Z\n" \
+             "DESCRIPTION:Happy birthday!\n" \
+             "LAST-MODIFIED:20211029T145140Z\n" \
+             "SEQUENCE:0\n" \
+             "STATUS:CONFIRMED\n" \
+             "SUMMARY:Happy birthday!\n" \
+             "TRANSP:OPAQUE\n" \
+             "END:VEVENT\n" \
+             "BEGIN:VEVENT\n" \
+             "DTSTART;VALUE=DATE:20210508\n" \
+             "DTEND;VALUE=DATE:20210509\n" \
+             "DTSTAMP:20211102T171105Z\n" \
+             "UID:2021_BIRTHDAY_self@google.com\n" \
+             "X-GOOGLE-CALENDAR-CONTENT-DISPLAY:chip\n" \
+             "X-GOOGLE-CALENDAR-CONTENT-ICON:https://calendar.google.com/googlecalendar/i\n" \
+             " mages/cake.gif\n" \
+             "CLASS:PUBLIC\n" \
+             "CREATED:20211029T145140Z\n" \
+             "DESCRIPTION:Happy birthday!\n" \
+             "LAST-MODIFIED:20211029T145140Z\n" \
+             "SEQUENCE:0\n" \
+             "STATUS:CONFIRMED\n" \
+             "SUMMARY:Happy birthday!\n" \
+             "TRANSP:OPAQUE\n" \
+             "END:VEVENT\n" \
+             "BEGIN:VEVENT\n" \
+             "DTSTART;VALUE=DATE:20220508\n" \
+             "DTEND;VALUE=DATE:20220509\n" \
+             "DTSTAMP:20211102T171105Z\n" \
+             "UID:2022_BIRTHDAY_self@google.com\n" \
+             "X-GOOGLE-CALENDAR-CONTENT-DISPLAY:chip\n" \
+             "X-GOOGLE-CALENDAR-CONTENT-ICON:https://calendar.google.com/googlecalendar/i\n" \
+             " mages/cake.gif\n" \
+             "CLASS:PUBLIC\n" \
+             "CREATED:20211029T145140Z\n" \
+             "DESCRIPTION:Happy birthday!\n" \
+             "LAST-MODIFIED:20211029T145140Z\n" \
+             "SEQUENCE:0\n" \
+             "STATUS:CONFIRMED\n" \
+             "SUMMARY:Happy birthday!\n" \
+             "TRANSP:OPAQUE\n" \
+             "END:VEVENT\n" \
+             "END:VCALENDAR"
 
 
 @pytest.fixture
@@ -60,15 +113,14 @@ async def test_import_file(bot):
     await test.message("!test_import")
     await asyncio.sleep(.25)
 
-def test_import_ics():
 
-    file = open(r"test/files/test.ics", "r")
-    gcal = Calendar.from_ical(file.read())
-    file.close()
+def test_import_ics():
+    gcal = Calendar.from_ical(ICS_STRING)
 
     data = get_ics_data(gcal)
 
     assert verify_csv(data)
+
 
 def test_import_ics_empty():
     gcal = Calendar.from_ical("BEGIN:VCALENDAR\n"
@@ -77,6 +129,7 @@ def test_import_ics_empty():
     data = get_ics_data(gcal)
 
     assert verify_csv(data)
+
 
 def test_time():
     old_time = "1998-05-08 10:30:00"
@@ -118,6 +171,7 @@ def test_typo_csv():
     table = pd.DataFrame(data=data)
 
     assert not verify_csv(table)
+
 
 def test_missing_column_csv():
     data = {'ID': [''],
