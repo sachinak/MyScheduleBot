c2fcf196cf46099387e023555c26804185492d8d
Sachin Kanth <sachin.kanth@wowlabz.com>
2023-11-26 17:44:54 -0500

    removed token trace

--- src/config.py
+++ src/config.py
@@ -2,4 +2,3 @@ import os
 TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "YOUR TOKEN HERE" 
 connection_string = "localhost:27017"
 db_name = "DB"
-# MTE3MTg4NzI5NTI3Njc5Mzg5OA.G2Rgcz.M8VCswFgGUodxBcPO2QwgKvm9A142yTOi1at_U
