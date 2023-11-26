71d0caf88bf08c922350c5edd4c9f79374a29984
Kabir Singh Bhatia <kabirsinghbhatia@gmail.com>
2023-11-26 18:37:36 -0500

    Refactored config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,3 @@
 import os
-TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "YOUR TOKEN HERE" 
-connection_string = "localhost:27017"
-db_name = "DB"
+TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "YOUR DISCORD BOT TOKEN" 
+CONNECTION_STRING = os.getenv("CONNECTION_STRING") if os.getenv("CONNECTION_STRING") else "YOUR DB CONNECTION STRING"
