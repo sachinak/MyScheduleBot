367365ed6b6d8a165200796ce76e17c4e78487e7
Sachin Kanth <sachin.kanth@wowlabz.com>
2023-11-26 17:43:49 -0500

    token removed from config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
-TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "MTE3MTg4NzI5NTI3Njc5Mzg5OA.GekHxy.YyYcYM0ieO7tS7v1tAERqp5i1-YR5lR1uMVNiI" 
+TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "YOUR TOKEN HERE" 
 connection_string = "localhost:27017"
 db_name = "DB"
+# MTE3MTg4NzI5NTI3Njc5Mzg5OA.G2Rgcz.M8VCswFgGUodxBcPO2QwgKvm9A142yTOi1at_U
