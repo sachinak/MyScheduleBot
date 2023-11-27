482cde223340a30a16bf38f3e69dcc1faf92fc7f
Kabir Singh Bhatia <kabirsinghbhatia@gmail.com>
2023-11-26 20:08:39 -0500

    Init db through docker-compose

--- docker-build/mongo-entrypoint/init_db.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Creating mongo users..."
-mongo admin --host localhost -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.createUser({user: '$HANDLER', pwd: '$HANDLER_PASSWORD', roles: [{role: 'readWrite', db: 'Schedulebot'}]})"
-echo "Mongo users created."
