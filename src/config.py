7881d5f9d0dd895c41ce8a16218abf4d6a57a068
Sachin Kanth <sachin.kanth@wowlabz.com>
2023-11-26 21:59:06 -0500

    mongo db in commit

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 import os
 TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "YOUR TOKEN HERE" 
-connection_string = "localhost:27017"
+connection_string = "mongodb://localhost:27017"
 db_name = "DB"
