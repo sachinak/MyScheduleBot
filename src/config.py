116bcf78ecbbac0f56227944ec6af283943bb0c3
sachinak <sachinkanth8@gmail.com>
2023-11-27 22:57:33 -0500

    Revert "Tenant fix"

--- src/config.py
+++
@@ -1,3 +0,0 @@
-import os
-TOKEN = os.getenv("TOKEN") if os.getenv("TOKEN") else "YOUR DISCORD BOT TOKEN" 
-CONNECTION_STRING = os.getenv("CONNECTION_STRING") if os.getenv("CONNECTION_STRING") else "YOUR DB CONNECTION STRING"
